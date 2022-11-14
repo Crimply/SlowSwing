f016a868152bdd00da59f11e25364cbfa3ea87fe
Crimply <>
2022-11-14 21:52:34 +1200

    V1.0
    Signed-off-by: Crimply <>

--- src/main/java/me/crimp/SlowSwing/SlowSwingMod.java
+++ src/main/java/me/crimp/SlowSwing/SlowSwingMod.java
@@ -2,11 +2,9 @@ package me.crimp.SlowSwing;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.lwjgl.opengl.Display;
 
 @Mod(modid = SlowSwingMod.MODID, name = SlowSwingMod.MODNAME, version = SlowSwingMod.MODVER)
 public class SlowSwingMod {
@@ -16,16 +14,6 @@ public class SlowSwingMod {
     public static final Logger LOGGER = LogManager.getLogger("SlowSwing");
     public static CapeManager capeManager;
 
-    public static void load() {
-        LOGGER.info("Loading SlowSwing by Crimp");
-        capeManager = new CapeManager();
-        LOGGER.info("Managers loaded.");
-        LOGGER.info("Modules loaded.");
-        LOGGER.info("EventManager loaded.");
-        LOGGER.info("SlowSwing successfully loaded!\n");
-    }
-
-
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOGGER.info("SlowSwing - Crimp");
@@ -33,13 +21,8 @@ public class SlowSwingMod {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        Display.setTitle("Minecraft 1.12.2");
-        SlowSwingMod.load();
+        capeManager = new CapeManager();
+        LOGGER.info("Loading SlowSwing by Crimp");
         }
-
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-            Display.setTitle("Minecraft 1.12.2");
-    }
 }
 
